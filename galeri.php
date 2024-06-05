da9ca6815cf7f6ae350b00f66506cf8482efa745
Tois286 <itoto1937@gmail.com>
2024-06-05 14:43:55 -0700

    gas

--- galeri.php
+++ galeri.php
@@ -1,29 +1,93 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style2.css">
     <title>Galeri Pondok</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/style2.css">
 </head>
+
 <body>
-    <h2>Galeri Pondok</h2>
+    <nav class="navbar navbar-expand-lg" style="background-color: white; border-radius: 10px;">
+        <div class="container">
+            <a class="navbar-brand" href="index.php"><img src="img/logo1.png" style="width: 200px;" alt="Logo"></a>
+            <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php" style="color:black;">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style="color:black;">Umum</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style="color:black;">Kegiatan</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style="color:black;">Dewan Asatidz</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav><br>
+
     <div class="wrapper">
-        <div class="box"><img src="img/DT.jpg" alt=""></div>
-        <div class="box"><img src="img/DT.jpg" alt=""></div>
-        <div class="box"><img src="img/DT.jpg" alt=""></div>
-        <div class="box"><img src="img/DT.jpg" alt=""></div>
-        <div class="box"><img src="img/DT.jpg" alt=""></div>
-        <div class="box"><img src="img/DT.jpg" alt=""></div>
-
-        <div class="box"><img src="img/DT.jpg" alt=""></div>
-        <div class="box"><img src="img/DT.jpg" alt=""></div>
-        <div class="box"><img src="img/DT.jpg" alt=""></div>
-        <div class="box"><img src="img/DT.jpg" alt=""></div>
-        <div class="box"><img src="img/DT.jpg" alt=""></div>
-        <div class="box"><img src="img/DT.jpg" alt=""></div>
-
-    
+        <div class="box"><img src="img/DT.jpg" alt="">
+            <p>keterangan gambar</p>
+        </div>
+        <div class="box"><img src="img/DT.jpg" alt="">
+            <p>keterangan gambar</p>
+        </div>
+        <div class="box"><img src="img/DT.jpg" alt="">
+            <p>keterangan gambar</p>
+        </div>
+        <div class="box"><img src="img/DT.jpg" alt="">
+            <p>keterangan gambar</p>
+        </div>
+        <div class="box"><img src="img/DT.jpg" alt="">
+            <p>keterangan gambar</p>
+        </div>
+        <div class="box"><img src="img/DT.jpg" alt="">
+            <p>keterangan gambar</p>
+        </div>
+        <div class="box"><img src="img/DT.jpg" alt="">
+            <p>keterangan gambar</p>
+        </div>
+        <div class="box"><img src="img/DT.jpg" alt="">
+            <p>keterangan gambar</p>
+        </div>
+        <div class="box"><img src="img/DT.jpg" alt="">
+            <p>keterangan gambar</p>
+        </div>
+        <div class="box"><img src="img/DT.jpg" alt="">
+            <p>keterangan gambar</p>
+        </div>
+        <div class="box"><img src="img/DT.jpg" alt="">
+            <p>keterangan gambar</p>
+        </div>
+        <div class="box"><img src="img/DT.jpg" alt="">
+            <p>keterangan gambar</p>
+        </div>
     </div>
+
+    <footer>
+        <?php include 'modul/footer.php'; ?>
+    </footer>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
 </body>
+
 </html>
