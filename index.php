04fe8785b79827cc56a9f3b9d0c3b961a208477b
Tois286 <itoto1937@gmail.com>
2024-06-06 01:30:16 -0700

    mantaps

--- index.php
+++ index.php
@@ -53,37 +53,7 @@ if (isset($_SESSION['username']) && isset($_SESSION['password'])) {
 	</heading>
 
 	<!-- Navbar -->
-	<div class="">
-		<nav class="container radius navbar navbar-expand-lg navbar-light " style="background-color: #ffffff;">
-			<div class="container-fluid">
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-
-				<div class="collapse navbar-collapse" id="navbarSupportedContent">
-					<div class="navbar-nav">
-						<a class="nav-link active" aria-current="page" href="index.php"><i class="bi bi-house-door"></i> Beranda</a>
-						<a class="nav-link " href="galeri.php"><i class="bi bi-images"></i> Galeri Pondok</a>
-						<a class="nav-link " href="pengumuman.php"><i class="bi bi-bell"></i>Pengumuman</a>
-						<div class="dropdown"><button class="dropbtn"><i class="bi bi-files"></i>pendidikan</button>
-							<div class="dropdown-content">
-								<a href="https://www.alghozali.ac.id/profil/taman-pendidikan-al-quran-TPQ">Taman Pendidikan Al-Qur'an</a>
-								<a href="https://www.alghozali.ac.id/profil/madrasah-ibtidaiyah-mi">Madrasah Ibtidaiyah (MI)</a>
-								<a href="https://www.alghozali.ac.id/profil/smp-islam">SMP Islam Al-Ghozali</a>
-								<a href="https://www.alghozali.ac.id/profil/sma-islam">SMA Islam Al-Ghozali</a>
-								<a href="https://www.alghozali.ac.id/informasi/brosur-kegiatan-dan-biaya">Biaya dan Kegiatan</a>
-							</div>
-						</div>
-						<a class="nav-link " href="pendaftaran.php"><i class="bi bi-person-plus"></i> Pendaftaran</a>
-					</div>
-				</div>
-				<form class="d-flex">
-					<a href="login.php" class="btn btn-outline-danger">Login</a>
-				</form>
-			</div>
-
-		</nav>
-	</div>
+	<?php include 'modul/navbar.php'; ?>
 
 	<!-- Konten -->
 	<div class="container mt-3">
