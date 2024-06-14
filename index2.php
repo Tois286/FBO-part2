706542f339942f71902da59b7ddbe8c1f4f34577
muhamdis29 <rifqiahmad.fz@gmail.com>
2024-06-15 05:33:59 +0700

    index2 done

--- index2.php
+++ index2.php
@@ -1,14 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="css/style3.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <title>Data Santri</title>
 </head>
-
 <body>
-    <a href="logout.php">Keluar</a>
-</body>
+    <div class="navigation">
+        <div class="menuToggle"></div>
+        <ul>
+            <li class="list active">
+                <a href="#" style="--clr:#f44336;">
+                    <span class="icon">
+                        <ion-icon name="home-outline"></ion-icon>
+                    </span>
+                    <span class="text">Home</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="#" style="--clr:#ffa117;">
+                    <span class="icon">
+                        <ion-icon name="person-outline"></ion-icon>
+                    </span>
+                    <span class="text">Person</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="#" style="--clr:#0fc70f;">
+                    <span class="icon">
+                        <ion-icon name="chatbubble-outline"></ion-icon>
+                    </span>
+                    <span class="text">Pengumuman</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="#" style="--clr:#2196f3;">
+                    <span class="icon">
+                        <ion-icon name="book-outline"></ion-icon>
+                    </span>
+                    <span class="text">Registrasi</span>
+                </a>
+            </li>
+            <div class="indicator"></div>
+        </ul>
+    </div>
 
+    <script src="css/script.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
 </html>
