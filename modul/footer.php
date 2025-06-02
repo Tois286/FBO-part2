cc34f447673929193d2b09f9f7b70d4db99b22a0
muhamdis29 <rifqiahmad.fz@gmail.com>
2025-06-02 23:53:51 +0700

    tailah

--- modul/footer.php
+++ modul/footer.php
@@ -1,4 +1,4 @@
 <div class="container-fluid my-3 text-center heading text-white">
     <p>Copyright @ 2023 <a href="https://www.instagram.com/daaruttazkia_official?igsh=MW95dDB0d2hhOTh0MA==" class=" text-white">PESANTREN DAARUT TAZKIA</a><br>
-        Create_By @ 2023 <a href="https://www.instagram.com/muhandisajaa?igsh=MXg3c3U0bWIyZDF6YQ==" class=" text-white">Rifqi Ahmad Fauzi</a></p>
+        Create_By @ 2023 <a href="https://www.instagram.com/muhandisajaa?igsh=MXg3c3U0bWIyZDF6YQ==" class=" text-white">Alumni Daarut Tazkia 01</a></p>
 </div>
